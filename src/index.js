6b18e1f833ba885c8efb9cf7f1c19295506b84b9
Osama farha <osamadsa130@gmail.com>
2026-02-15 13:46:42 +0300

    ed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/globals.css';
 import App from './App';
+import React from 'react';
+import { HashRouter } from 'react-router-dom';  // استيراد HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>  {/* لف الـ App بـ HashRouter */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
